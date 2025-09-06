b2e8af11c158f415a140dc10d3c5c0a685facdd5
Baruk Ontiveros <barukramirez297@gmail.com>
2025-09-05 18:47:23 -0600

    Segundo commit 05-09-2025

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+console.log("Hola Mundos")
+
+var num1=23;
+let num2=32;
+const num3=67;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+let sum = num1 + num2 + num3;
+console.log("La suma es:"+sum)
+
+let matricula: string|number
+
+matricula=123456789
+matricula="YH11177850"
+console.log(matricula)
